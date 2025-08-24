4b5132b26c383343a39491bc9d4ded00d5e646b3
Joni Pahikainen <joni.pahikainen01@gmail.com>
2025-08-24 11:54:27 +0300

    chore: temporarily remove handicap and statistics features

--- backend/src/db/repositories/index.ts
+++ backend/src/db/repositories/index.ts
@@ -1,5 +1,3 @@
 export * from "./courses";
 export * from "./rounds";
 export * from "./users";
-export * from "./statistics";
-export * from "./handicap";
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,8 +3,6 @@ import authRoutes from './routes/auth';
 import userRoutes from './routes/users';
 import courseRoutes from './routes/courses';
 import roundRoutes from './routes/rounds';
-import statisticsRoutes from './routes/statistics';
-import handicapRoutes from './routes/handicap';
 import cors from "cors";
 
 const app = express();
@@ -18,8 +16,6 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/courses', courseRoutes);
 app.use('/api/rounds', roundRoutes);
-app.use('/api/statistics', statisticsRoutes);
-app.use('/api/handicap', handicapRoutes);
 
 app.get('/', (req, res) => {
   res.send('Profile Service is running');
