f4f49e1f507d34f7dfbca94c762a6e79cb29bd12
Joni Pahikainen <joni.pahikainen01@gmail.com>
2025-08-25 22:12:07 +0300

    test: export app for Jest testing without running server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -21,6 +21,11 @@ app.get('/', (req, res) => {
   res.send('Profile Service is running');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+}
+
+export { app };
